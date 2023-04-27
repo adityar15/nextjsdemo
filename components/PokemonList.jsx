e9eebdd48f5362254e26c7bcae39e9ab6f98e4d3
adityar15 <adityask2194@gmail.com>
2023-04-27 18:23:47 +0100

    pokemon api connection

--- components/PokemonList.jsx
+++ components/PokemonList.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
+import React from "react"
+import Link from "next/link"
 
-function PokemonListItem({pokemon}) {
-    return(
-        <li className="border border-gray-400 p-5 flex items-center space-x-4">
-            {/* dot */}
-            <span className="text-gray-700 text-xl">{pokemon.name}</span>
-        </li>
-        )
+
+const colorMap = {
+  fire: "bg-red-500",
+  water: "bg-blue-500",
+  grass: "bg-green-500",
 }
 
+function PokemonListItem({ pokemon }) {
+  return (
+   <Link href={`/pokemons/${pokemon.name}`}>
+    <li className="border border-gray-400 p-5 flex flex-col">
+      <span className="flex items-center space-x-4">
+        <div className={`w-20 h-20 text-gray-50 rounded-full grid place-items-center ${colorMap[pokemon.type1]}`}>
+           <span>
+           {pokemon.type1}
+           </span>
+        </div>
+        <span className="text-gray-700 text-xl">{pokemon.name}</span>
+      </span>
+      <span className="text-gray-700"> {pokemon.classfication}</span>
+    </li>
+   </Link>
+  )
+}
 
-export default function PokemonList({pokemons}) {
+export default function PokemonList({ pokemons }) {
   return (
     <ul>
-        {
-            pokemons.length > 0 ? 
-                pokemons.map((pokemon, index)=> <PokemonListItem pokemon={pokemon} key={index} />)
-            : <h1 className="text-3xl text-gray-600"> No pokemons in database </h1> 
-        }
+      {pokemons.length > 0 ? (
+        pokemons.map((pokemon, index) => <PokemonListItem pokemon={pokemon} key={index} />)
+      ) : (
+        <h1 className="text-3xl text-gray-600"> No pokemons in database </h1>
+      )}
     </ul>
   )
 }
--- pages/pokemons/index.js
+++ pages/pokemons/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Head from "next/head"
-import pokemons from "@/assets/sampledataset.json"
+// import pokemons from "@/assets/sampledataset.json"
 import PokemonList from "@/components/PokemonList"
 import Title from "@/components/Title"
 
@@ -20,13 +20,16 @@ export default function Pokemons({pokemonsList}) {
 }
 
 export async function getServerSideProps(context) {
-  console.log("pokemon", pokemons)
-  // const response = await fetch(url)
-  // const data = await response.json()
+  // console.log("pokemon", pokemons)
+  const response = await fetch("http://127.0.0.1:8000/pokemon")
+  const data = await response.json()
+
+
+  console.log("pokemons", data)
 
   return {
     props: {
-      pokemonsList: pokemons,
+      pokemonsList: data,
     },
   }
 }
